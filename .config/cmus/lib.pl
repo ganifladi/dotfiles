1e428e871166b21b4c9c41ac2435c3107c4e44a5
ganifladi <lokus06@web.de>
2017-11-22 16:23:03 +0100

    added cmus config

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,26 @@
+/home/rene/Musik/Musik/MONGOL800/メッセージ/01 あなたに.mp3
+/home/rene/Musik/Musik/MONGOL800/メッセージ/02 Ｓｏｎｇ　ｆｏｒ　ｙｏｕ.mp3
+/home/rene/Musik/Musik/MONGOL800/メッセージ/03 小さな恋のうた.mp3
+/home/rene/Musik/Musik/MONGOL800/メッセージ/04 Ｍｅｌｏｄｙ.mp3
+/home/rene/Musik/Musik/MONGOL800/メッセージ/05 月灯りの下で.mp3
+/home/rene/Musik/Musik/MONGOL800/メッセージ/06 Ｆｏｒ　Ｌｉｆｅ.mp3
+/home/rene/Musik/Musik/MONGOL800/メッセージ/07 親知らず－Ｓｕｍｍｅｒ　Ａｇａｉｎ－.mp3
+/home/rene/Musik/Musik/MONGOL800/メッセージ/08 ＨＥＹ　Ｍｏｍｍｙ.mp3
+/home/rene/Musik/Musik/MONGOL800/メッセージ/09 Ｍａｒｒｉａｇｅ　Ｂｌｕｅ.mp3
+/home/rene/Musik/Musik/MONGOL800/メッセージ/10 矛盾の上に咲く花.mp3
+/home/rene/Musik/Musik/MONGOL800/メッセージ/11 琉球愛歌.mp3
+/home/rene/Musik/Musik/MONGOL800/メッセージ/12 Ｄｅａｒ　Ｍｙ　Ｌｏｖｅｒｓ.mp3
+/home/rene/Musik/Musik/MONGOL800/メッセージ/13 夢叶う.mp3
+/home/rene/Musik/Musik/MONGOL800/メッセージ/14 Ｄａｎｄｅｌｉｏｎ.mp3
+/home/rene/Musik/Musik/MONGOL800/エイト・ハンドレッド/01 神様.mp3
+/home/rene/Musik/Musik/MONGOL800/エイト・ハンドレッド/02 ターコイズ.mp3
+/home/rene/Musik/Musik/MONGOL800/エイト・ハンドレッド/03 Ｂｒｅａｋｆａｓｔ＠曙.mp3
+/home/rene/Musik/Musik/MONGOL800/エイト・ハンドレッド/04 常夏.mp3
+/home/rene/Musik/Musik/MONGOL800/エイト・ハンドレッド/05 Ｎｏ　ｗｏｒｋｉｎｇ　ｍａｎ　ｓｋａ.mp3
+/home/rene/Musik/Musik/MONGOL800/エイト・ハンドレッド/06 ｎｏｔ　ｅｑｕａｌ.mp3
+/home/rene/Musik/Musik/MONGOL800/エイト・ハンドレッド/07 ｂｌｕｅ　ｂｌｕｅｓ.mp3
+/home/rene/Musik/Musik/MONGOL800/エイト・ハンドレッド/08 Ｉ’ｍ　ｉｎ　ｌｏｖｅ.mp3
+/home/rene/Musik/Musik/MONGOL800/エイト・ハンドレッド/09 Ｉ’ｌｌ　ｂｅ.mp3
+/home/rene/Musik/Musik/MONGOL800/エイト・ハンドレッド/10 Ｒｏｃｋｉｎ’　ｍｏｃｋ.mp3
+/home/rene/Musik/Musik/MONGOL800/エイト・ハンドレッド/11 ベイビーシーサー.mp3
+/home/rene/Musik/Musik/MONGOL800/エイト・ハンドレッド/12 Ｈａｐｐｙ　ｅｎｄ.mp3
