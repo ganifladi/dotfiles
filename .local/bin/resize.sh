b6f0635e886f36587c0b883666a74ec843c70060
ganifladi <lokus06@web.de>
2017-12-21 21:58:05 +0100

    added some options

--- .local/bin/resize.sh
+++ .local/bin/resize.sh
@@ -1,43 +1,57 @@
 #!/bin/bash
 
+VID=0;
+PIC=0;
 PICSIZE=1920x1080
 
-for PICTURE in *.jpg; do
-    if [[ "$PICTURE" == x-*.jpg ]]; then
-        # echo success;
-        echo $PICTURE has already been resized;
-    else
-        # echo fail;
-        convert $PICTURE -resize $PICSIZE x-$PICTURE;
-    fi
-done
+case $1 in
+    video) VID=1;;
+    picture) PIC=1;;
+esac
 
-for PICTURE in *.png; do
-    if [[ "$PICTURE" == x-*.png ]]; then
-        # echo success;
-        echo $PICTURE has already been resized;
-    else
-        # echo fail;
-        convert $PICTURE -resize $PICSIZE x-$PICTURE;
-    fi
-done
+if [ "$PIC" -eq 1 ]; then
+    for PICTURE in *.jpg; do
+        if [[ "$PICTURE" == x-*.jpg ]]; then
+            # echo success;
+            echo $PICTURE has already been resized;
+        else
+            # echo fail;
+            convert $PICTURE -resize $PICSIZE x-$PICTURE;
+        fi
+    done
 
-for VIDEO in *.mp4; do
-    if [[ "$VIDEO" == x-*.mp4 ]]; then
-        # echo success;
-        echo $VIDEO has already been resized;
-    else
-        # echo fail;
-        ffmpeg -i $VIDEO -acodec copy -vcodec libx264 -crf 40 "x-${VIDEO}"
-    fi
-done
+    for PICTURE in *.png; do
+        if [[ "$PICTURE" == x-*.png ]]; then
+            # echo success;
+            echo $PICTURE has already been resized;
+        else
+            # echo fail;
+            convert $PICTURE -resize $PICSIZE x-$PICTURE;
+        fi
+    done
 
-for VIDEO in *.MOV; do
-    if [[ "$VIDEO" == x-*.mp4 ]]; then
-        # echo success;
-        echo $VIDEO has already been resized;
-    else
-        # echo fail;
-        ffmpeg -i $VIDEO -acodec copy -vcodec libx264 -crf 40 "x-${VIDEO}"
-    fi
-done
+fi
+
+if [ "$VID" -eq 1 ]; then
+
+    for VIDEO in *.mp4; do
+        if [[ "$VIDEO" == x-*.mp4 ]]; then
+            # echo success;
+            echo $VIDEO has already been resized;
+        else
+            # echo fail;
+            ffmpeg -i $VIDEO -acodec copy -vcodec libx264 -crf 40 "x-${VIDEO}"
+        fi
+    done
+
+    for VIDEO in *.MOV; do
+        if [[ "$VIDEO" == x-*.mp4 ]]; then
+            # echo success;
+            echo $VIDEO has already been resized;
+        else
+            # echo fail;
+            ffmpeg -i $VIDEO -acodec copy -vcodec libx264 -crf 40 "x-${VIDEO}"
+        fi
+    done
+
+fi
